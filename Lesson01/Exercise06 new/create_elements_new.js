ee4f1dc68047c32d5b8b166ea824ad5b3addb5c1
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-07-16 11:19:22 +0100

    We created the root container and added a the class for it.
    Then we started by generating the HTML for the tag as a string and returns an HTML element for it. We use that to map all tags to elements using the map function on the tags array. Then we use that to map all tags to elements using the map function on the tags array.
    
    Then we mapped the element to HTML by using the outerHTML attribute from it.
    
    With the container element created and the HTML for the tags ready, the innerHTML attribute of the content we set using a template string and returned it.

--- Lesson01/Exercise06 new/create_elements_new.js
+++ Lesson01/Exercise06 new/create_elements_new.js
@@ -1,5 +1,17 @@
-/**
- * 1, Create the container element and add a class to it.
- * 2, Create the HTML for the inner content using a string templete.
- * 3, Set innerHTML on the container element.
- */
+// 1, Create the container element and add a class to it.
+function createContent(product) {
+    const content = document.createElement("div");
+    content.className = "content";
+
+    // 2, Create the HTML for the inner content using a string templete.
+    const tagsHTML = product.tags.map(createTagElement).map(el => el.outerHTML).join("");
+
+    // 3, Set innerHTML on the container element.
+    content.innerHTML = `
+        <a class="header">${product.name}</a>
+        <div class="meta"><span>$${product.price} / ${product.unit}</span></div>
+        <div class="extra">${tagsHTML}</div>
+    `;
+
+    return content;
+}
