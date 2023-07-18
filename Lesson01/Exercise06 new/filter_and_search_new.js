9a4084374b63182f44c5d284cae5fa599fb32492
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-07-18 23:22:41 +0100

    applyFilters  is just a catch all function that will contain all the logic related to updating the page when the filtering condition change.

--- Lesson01/Exercise06 new/filter_and_search_new.js
+++ Lesson01/Exercise06 new/filter_and_search_new.js
@@ -1,14 +1,25 @@
 const tagsToFilterBy = [];
 let textToSearch = "";
 
+// func to add all the tags in the page
 function AddFilterTags() {
     Array.from(document.querySelectorAll(".extra .label")).forEach((tagEl) => {
         tagEl.addEventListener("click", () => {
-            
+
             if (!tagsToFilterBy.includes(tagEl.innerHTML)) {
                 tagsToFilterBy.push(tagEl.innerHTML);
-                applyFIlters();
+                applyFilters();
             }
+
         });
     });
 }
+
+// catch all func that will contain all the logic related to updating the page
+function applyFilters() {
+    createListForProducts(filterByText(filterByTags(products)));
+    addTagFilter();
+    updateTagFilterList();
+}
+
+
